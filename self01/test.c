2c8ae1cfff0e807f881d740790888b0785c4991c
超 张 <imagine243@gmail.com>
2016-09-27 21:54:39 +0800

    refactor test

--- self01/test.c
+++ self01/test.c
@@ -7,77 +7,72 @@ static int main_ret = 0;
 static int test_count = 0;
 static int test_pass = 0;
 
-#define EXPECT_EQ_BASE(equality, expect, actual, format) \
-do {\
-test_count++;\
-if (equality)\
-test_pass++;\
-else {\
-fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
-main_ret = 1;\
-}\
-} while(0)
+#define EXPECT_EQ_BASE(equality, expect, actual, format)                                                           \
+    do                                                                                                             \
+    {                                                                                                              \
+        test_count++;                                                                                              \
+        if (equality)                                                                                              \
+            test_pass++;                                                                                           \
+        else                                                                                                       \
+        {                                                                                                          \
+            fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual); \
+            main_ret = 1;                                                                                          \
+        }                                                                                                          \
+    } while (0)
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
+#define EXPECT_UNEQ_INT(expect, actual) EXPECT_EQ_BASE((expect) != (actual), expect, actual, "%d")
+
+#define TEST_ERROR(error, json)                        \
+    do                                                 \
+    {                                                  \
+        lept_value v;                                  \
+        v.type = LEPT_ERROR;                           \
+        EXPECT_UNEQ_INT(error, lept_parse(&v, json));  \
+        EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));   \
+    } while (0)
+
+#define TEST_RIGHT(right, json, value)              \
+    do                                              \
+    {                                               \
+        lept_value v;                               \
+        v.type = LEPT_ERROR;                        \
+        EXPECT_EQ_INT(right, lept_parse(&v, json)); \
+        EXPECT_EQ_INT(value, lept_get_type(&v));    \
+    } while (0)
+
 static void test_parse_null()
 {
-    lept_value v;
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+    TEST_RIGHT(LEPT_PARSE_OK, "null", LEPT_NULL);
 }
 
 static void test_parse_true()
 {
-    lept_value v;
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
-    EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
+    TEST_RIGHT(LEPT_PARSE_OK, "true", LEPT_TRUE);
 }
 
 static void test_parse_false()
 {
-    lept_value v;
-    v.type = LEPT_NULL;
-    EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
-    EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
-    
+    TEST_RIGHT(LEPT_PARSE_OK,"false", LEPT_FALSE);
 }
 
-
-static void test_parse_expect_value() {
-    lept_value v;
-    
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, ""));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
-    
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_EXPECT_VALUE, lept_parse(&v, " "));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+static void test_parse_expect_value()
+{
+    TEST_RIGHT(LEPT_PARSE_EXPECT_VALUE, "", LEPT_NULL);
+    TEST_RIGHT(LEPT_PARSE_EXPECT_VALUE, " ", LEPT_NULL);
 }
 
-static void test_parse_invalid_value() {
-    lept_value v;
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nul"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
-    
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "?"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+static void test_parse_invalid_value()
+{
+    TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, "nul");
+    TEST_ERROR(LEPT_PARSE_EXPECT_VALUE, "?");
 }
 
-
-static void test_parse_root_not_singular() {
-    lept_value v;
-    v.type = LEPT_FALSE;
-    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "null x"));
-    EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+static void test_parse_root_not_singular()
+{
+    TEST_RIGHT(LEPT_PARSE_ROOT_NOT_SINGULAR, "null x", LEPT_NULL);
 }
-
-
 
 static void test_parse()
 {
@@ -89,7 +84,8 @@ static void test_parse()
     test_parse_root_not_singular();
 }
 
-int main() {
+int main()
+{
     test_parse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
     return main_ret;
