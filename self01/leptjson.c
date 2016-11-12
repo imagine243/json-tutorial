127a557e8bfc067d2aa7a28d0335f31fc45cdd13
chao <chao@DESKTOP-GV2RUJ4>
2016-11-12 20:48:01 +0800

    add literal

--- self01/leptjson.c
+++ self01/leptjson.c
@@ -28,46 +28,21 @@ static void lept_parse_whitespace(lept_context *c)
     c->json = p;
 }
 
-static int lept_perser_literal(lept_context* c, lept_value* v, const char* literal, lept_type type)
+static int lept_perse_literal(lept_context* c, lept_value* v, const char* literal, lept_type type)
 {
     size_t i;
     EXPECT(c, literal[0]);
-}
+	for (i = 0; literal[i + 1]; i++)
+	{
+		if (c->json[i] != literal[i])
+		{
+			return LEPT_PARSE_INVALID_VALUE;
+		}
+	}
 
-static int lept_parse_null(lept_context *c, lept_value *v)
-{
-    EXPECT(c, 'n');
-    if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
-    {
-        return LEPT_PARSE_INVALID_VALUE;
-    }
-    c->json += 3;
-    v->type = LEPT_NULL;
-    return LEPT_PARSE_OK;
-}
-
-static int lept_parse_true(lept_context *c, lept_value *v)
-{
-    EXPECT(c, 't');
-    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
-    {
-        return LEPT_PARSE_INVALID_VALUE;
-    }
-    c->json += 3;
-    v->type = LEPT_TRUE;
-    return LEPT_PARSE_OK;
-}
-
-static int lept_parse_false(lept_context *c, lept_value *v)
-{
-    EXPECT(c, 'f');
-    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e')
-    {
-        return LEPT_PARSE_INVALID_VALUE;
-    }
-    c->json += 4;
-    v->type = LEPT_FALSE;
-    return LEPT_PARSE_OK;
+	c->json += i;
+	v->type = type;
+	return LEPT_PARSE_OK;
 }
 
 static int lept_parse_number(lept_context* c, lept_value* v)
@@ -119,11 +94,11 @@ static int lept_parse_value(lept_context *c, lept_value *v)
     switch (*c->json)
     {
     case 'n':
-        return lept_parse_null(c, v);
+		return lept_perse_literal(c, v, "null", LEPT_NULL);
     case 't':
-        return lept_parse_true(c, v);
+		return lept_perse_literal(c, v, "true", LEPT_TRUE);
     case 'f':
-        return lept_parse_false(c, v);
+		return lept_perse_literal(c, v, "false",LEPT_FALSE);
     case '\0':
         return LEPT_PARSE_EXPECT_VALUE;
     default:
