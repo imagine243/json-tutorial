aeb37aad5311163a3fb971fce14ee977afef20f1
张超 <imagine243>
2017-03-13 17:38:16 +0800

    add perse (true false)

--- self01/test.c
+++ self01/test.c
@@ -24,14 +24,33 @@ static void test_parse_null() {
 	lept_value v;
 	v.type = LEPT_FALSE;
 	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
+	EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
+}
+
+static void test_parse_true() {
+	lept_value v;
+	v.type = LEPT_FALSE;
+	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "true"));
+	EXPECT_EQ_INT(LEPT_TRUE, lept_get_type(&v));
+}
+
+static void test_parse_false() {
+	lept_value v;
+	v.type = LEPT_NULL;
+	EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "false"));
+	EXPECT_EQ_INT(LEPT_FALSE, lept_get_type(&v));
 }
 
 static void test_parse()
 {
 	test_parse_null();
+	test_parse_true();
+	test_parse_false();
 }
 
 int main() {
 	test_parse();
 	printf("%d/%d (%3.2f%%) passed \n", test_pass, test_count, test_pass * 100.0 / test_count);
+
+	return 0;
 }
